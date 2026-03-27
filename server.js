a77fccbced72fbec8ed37c8acf98ea2f9b2a878f
ProMetaV <s.hoffmann.marquez@gmail.com>
2026-03-27 09:20:37 +0000

    Initial ProMetaV website deploy

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/onboarding', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'onboarding.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`ProMetaV server running on port ${PORT}`);
+});
